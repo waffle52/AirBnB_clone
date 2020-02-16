cab8c18596cf2bfde55dc5088c88c7afdce60d9f
waffle <asuka9767@protonmail.com>
2020-02-16 00:58:51 +0000

    fixed to scope of project and add documentation

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,31 @@
 from datetime import datetime
 from uuid import uuid4
 
+
 class BaseModel:
     """ Class BaseModel """
 
     def __init__(self):
+        """ initialize variables """
         self.id = str(uuid4())
-        self.created_at = str(datetime.today())
-        self.updated_at = str(datetime.today())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        """ return string with info on class """
+        name = self.__class__.__name__
+        return ("[{}] ({}) {}".format(name, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = str(datetime.today())
+        """ func to update time """
+        self.updated_at = datetime.today()
 
     def to_dict(self):
+        """ func to createa dictionary of of class elements """
         mydict = dict(self.__dict__)
-        mydict.update( { "__class__" : "BaseModel"})
+        mydict.update({"__class__": "BaseModel"})
+        created = mydict.get("created_at").strftime("%Y-%m-%dT%H:%M:%S.%f")
+        mydict.update({"created_at": created})
+        update = mydict.get("updated_at").strftime("%Y-%m-%dT%H:%M:%S.%f")
+        mydict.update({"updated_at": update})
         return (mydict)

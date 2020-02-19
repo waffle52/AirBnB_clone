ea5b01063753eaad3e9697ef0246bf73d8463533
waffle <asuka9767@protonmail.com>
2020-02-19 22:41:42 +0000

    updated to add empty line

--- console.py
+++ console.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-import cmd, sys
+""" Console file """
+import cmd
+import sys
+from models.base_model import BaseModel
+from models import storage
+import shlex
+
 
 class HBNBCommand(cmd.Cmd):
-    intro = "hello"
+    """ Command class """
     prompt = '(hbnb) '
     file = None
 
@@ -12,7 +18,88 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         'Quit command to exit the program'
-        return True
+        raise SystemExit
+
+    def emptyline(self):
+        pass
+
+    """def do_create(self, arg):
+        'Create BaseModel instance'
+        if arg == "":
+            print("** class name missing **")
+        elif arg != BaseModel.__name__:
+            print("** class doesn't exist **")
+        else:
+            my_model = BaseModel()
+            print(my_model.id)
+            my_model.save()"""
+
+    """def do_show(self, arg):
+        new_list = shlex.split(arg)
+        all_objs = storage.all()
+        if new_list[0] == "":
+            print ("** class name missing **")
+        elif new_list[1] == "":
+            print ("** instance id missing **")
+        elif id != "":
+            print ("** no instance found **")
+        elif id != "":
+            print ("** class doesn't exist **")
+        else:
+            all_objs = storage.all()
+            word = arg + id
+            obj_id = all_objs[word]
+            print ("test: {}".format(obj_id))"""
+
+    """def do_destroy(self, arg):
+        #num = dict(arg.split(" "))
+        #arg = arg.split(" ")[0]
+        print("test: {}".format(num))
+        if arg == "" or arg is None:
+            print ("** class name missing **")
+        elif num == "":
+            print ("** instance id missing **")
+        all_objs = storage.all()
+        cls = False
+        ide = False
+        for obj_id in all_objs.keys():
+            if (arg == obj_id.split(".")):
+                cls = True
+                if (num == obj_id.split(".")[1]):
+                    ide = True
+                    del all_objs[obj_id]
+                    my_model.save()
+        if (cls == False):
+            print ("** class doesn't exist **")
+        elif (ide == False):
+            print ("** no instance found **")
+        """
+
+    """def do_all(self, arg):
+        if arg != "":
+            truth = True
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                if (arg in obj_id.split(".")):
+                    if (truth is True):
+                        print("[\"", end="")
+                    obj = all_objs[obj_id]
+                    print(obj, end="")
+                    truth = False
+            if (truth is True):
+                print("** class doesn't exist **")
+            else:
+                print("\"]")
+        else:
+            all_objs = storage.all()
+            print("[\"", end="")
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj, end="")
+            print("\"]")"""
+
+    """def do_update(self, arg):
+       """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
